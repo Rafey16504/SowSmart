3028cf0220015e846c52826711cc6ca3a35d5e85
Rafey16504 <abdulrafey12@yahoo.com>
2025-02-11 00:29:04 +0500

    Tailwind working now

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.tsx
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-green-500 text-white text-3xl font-bold">
+      Tailwind is working! 🚀
     </div>
   );
 }
-
-export default App;
