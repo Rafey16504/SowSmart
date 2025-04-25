bd0b0ed18d98b30db3782c26449eb386cb421da2
Rafey16504 <abdulrafey12@yahoo.com>
2025-04-25 18:59:40 +0500

    Added crop insights and made some fixes

--- node_app/src/index.ts
+++ node_app/src/index.ts
@@ -12,14 +12,14 @@ import { cropRecommend } from "./recommendation-api/crop-recommendation";
 import { getFarmer } from "./farmer-signup/check-existing";
 import { aiModel } from "./ai-chatbot/open-ai";
 import { diseaseRouter } from "./disease-api/disease_detection";
-import { cropInsightsRouter } from "./recommendation-api/crop-insights";
+import { cropInsightsRouter } from "./crop-insights/crop-insights";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json({ limit: "25mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 
